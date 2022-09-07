1fda8d089aadb785ff18e18509b37dd2906eb677
Ismael <ismael@MacBook-Air-de-Ismael.local>
2022-09-07 16:29:04 -0500

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import nameMaestro from "./slices/nameMaestro.slice";
+
+export default configureStore({
+    reducer:{
+        nameMaestro,
+    }
+})
